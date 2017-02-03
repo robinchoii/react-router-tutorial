0adfca7396c74c111d70bd7366a68d2a08e6d619
robinchoii <robinchoii@gmail.com>
2017-02-03 13:11:21 -0800

    steps 1-5 of tutorial, params

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import { Router, Route, hashHistory} from 'react-router'
+import About from  './modules/About'
+import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+
+render((
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <Route path='/repos' component={Repos} />
+      <Route path='/repos/:userName/:repoName' component={Repo} />
+      <Route path='/about' component={About} />
+    </Route>
+  </Router>
+  ), document.getElementById('app')
+)
+
